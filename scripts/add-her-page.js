3211119ac61281cf996ab6aa77d4484a0695cb46
Loren West <github@lorenwest.com>
2017-03-01 17:42:38 -0800

    Connecting the dots

--- scripts/add-her-page.js
+++ scripts/add-her-page.js
@@ -1,1 +1,7 @@
-alert('Add-her-page js loaded');
+setTimeout(function(){
+  var f = document.querySelector('.Main-content form');
+  f.onsubmit = function() {
+    console.log('caught');
+    return false;
+  }
+}, 1);
