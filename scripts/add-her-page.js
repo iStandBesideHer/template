a5a3ff1b2112b23b77899deebf72e82286600abb
Loren West <github@lorenwest.com>
2017-03-01 18:43:45 -0800

    Updated add page

--- scripts/add-her-page.js
+++ scripts/add-her-page.js
@@ -1,7 +1,41 @@
-setTimeout(function(){
-  var f = document.querySelector('.Main-content form');
-  f.onsubmit = function() {
-    console.log('caught');
-    return false;
+// Custom code for posting a new /i-stand-beside page
+var jq2 = jQuery.noConflict();
+jq2(function($) {
+
+  var f = $('.Main-content form');
+  var scrapeForm = function() {
+    fHerName = f.find('input[type=text]');
+    fPagePW = f.find('input[type=password]');
+    var form = {
+      herName: fHerName.val(),
+      pagePW: fPagePW.val()
+    }
+    fHerName.css({borderColor:(form.herName ? '#ccc' : 'red')});
+    fPagePW.css({borderColor:(form.pagePW ? '#ccc' : 'red')});
+    console.log(form);
+    if (!form.herName) {
+      fHerName.focus();
+      return null;
+    }
+    if (!form.pagePW) {
+      fPagePW.focus();
+      return null;
+    }
+    return form;
   }
-}, 1);
+
+  f.attr('onsubmit',null).submit(function(){
+    console.log('onSubmit');
+    var fields = scrapeForm();
+    if (fields) {
+      $.post('https://c784rk69oh.execute-api.us-east-1.amazonaws.com/prod/her-page', fields, function(d) {
+        alert("SUCCESS: " + JSON.stringify(d));
+      })
+      .fail(function(e) {
+        alert("An error occurred, please try again later.");
+      });
+    }
+    return false;
+  });
+
+});
